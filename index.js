7e25d1736ace647b317040b8e7230f6ae0b8dc45
QuantumSheep <quantumsheep@outlook.fr>
2018-12-18 14:04:25 +0100

    Remove commented socket.io require

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-// const io = require('socket.io')(http);
 const WebSocket = require("ws");
 const path = require("path");
 
