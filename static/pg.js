a16735de41f70c50111f41d04214814b4bb4f6d7
QuantumSheep <quantumsheep@outlook.fr>
2018-12-18 16:17:04 +0100

    Add WSS

--- static/pg.js
+++ static/pg.js
@@ -3,7 +3,7 @@ const pg_chat = document.getElementById('pg_chat');
 const pg_display = pg.querySelector('[data-desc="input"]');
 const pg_prompt = pg.querySelector('[data-desc="prompt"]');
 
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`ws${window.location.protocol.startsWith('https') ? 's' : ''}://${window.location.host}`);
 let nickname = 'anonymous';
 
 pg.addEventListener('click', function (e) {
